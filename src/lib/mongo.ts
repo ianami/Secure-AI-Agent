6d78ead38ebf7b7befe3b70b55f7a4c0cb4e45f7
Anamaria Beatrice <47271854+ianami@users.noreply.github.com>
2026-02-07 17:17:04 +0000

    Update mongo.ts

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -1,1 +1,29 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+const dbName = process.env.MONGODB_DB || "secure_ai_agent";
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri);
+  global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export async function getDb() {
+  const client = await clientPromise;
+  return client.db(dbName);
+}
 
